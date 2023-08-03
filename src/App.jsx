029ee469a90545bd46a09705cdbbaa5cb85f10eb
Aditya <adityarpadekar@gmail.com>
2023-08-04 00:18:26 +0530

    partial task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React, { Suspense, lazy, useEffect, useState } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
-import { getButtonsData } from "./Server/getButtons";
+// import { getButtonsData } from "./Server/getButtons";
 import { getUsersData } from "./Server/getUsersData";
 import {
-  AddButton,
-  Footer,
-  Landing,
-  Loader,
-  Login,
-  Main,
-  Navbar,
-  Score,
-  ExploreButtons
+    AddButton,
+    Footer,
+    Landing,
+    Loader,
+    Login,
+    Main,
+    Navbar,
+    Score,
+    ExploreButtons,
 } from "./components";
 import ErrorPage from "./components/ErrorPage/404Error";
 import SuspenseLoader from "./components/SuspenseLoader/SuspenseLoader";
@@ -19,110 +19,216 @@ import GoToTop from "./components/Top/GoToTop";
 import UserProfile from "./components/UserProfile/UserProfile";
 import About from "./pages/AboutUs/About";
 
+// changes by aditya
+
+import { db } from "./firebase/auth";
+import {
+    collection,
+    getDocs,
+    limit,
+    query,
+    startAfter,
+    orderBy,
+    startAt,
+    getCountFromServer,
+} from "firebase/firestore";
+
+// changes by aditya
+
 const ShowCode = lazy(() => import("./components/ShowCode/ShowCode"));
 
 const App = ({ modeToggleFunc, modeToggle }) => {
-  const [loading, setLoading] = useState(false);
-  const [toggleMode, setToggleMode] = useState(true);
+    const [loading, setLoading] = useState(false);
+    const [toggleMode, setToggleMode] = useState(true);
 
-  const location = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
+    const location = useLocation();
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
 
-  const [buttonsData, setButtonsData] = useState([]);
-  const [usersData, setUsersData] = useState([]);
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const buttonsData = await getButtonsData();
-        const usersData = await getUsersData();
-        setButtonsData(buttonsData);
-        setUsersData(usersData);
-      } catch (error) {
-        console.log("Error fetching data:", error);
-      }
-    }
-    fetchData();
-  }, []);
+    const [buttonsData, setButtonsData] = useState([]);
+    const [usersData, setUsersData] = useState([]);
 
-  const routes = [
-    {
-      path: "/",
-      element: (
+    // changes by aditya
+    const [lastButton, setLastButton] = useState();
+    const [totalBtn, setTotalBtn] = useState();
+
+    // const BUTTONS_CACHE_KEY = "buttonsData";
+    const getButtonsData = async () => {
+        // const cachedData = localStorage.getItem(BUTTONS_CACHE_KEY);
+        // if (cachedData) {
+        //     return JSON.parse(cachedData);
+        // }
+
+        // const buttonCollectionRef = collection(db, "buttons");
+        // const data = await getDocs(buttonCollectionRef);
+
+        const buttonCollectionRef = collection(db, "buttons");
+        let snapshot = query(
+            buttonCollectionRef,
+            orderBy("likeCounter", "desc"),
+            limit(6)
+        );
+        const data = await getDocs(snapshot);
+
+        const buttonsData = data.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+        }));
+
+        const cnt = await getCountFromServer(buttonCollectionRef);
+        buttonsData.map((item) => {
+            console.log("btnnnnnnnn----datttt", item.githubUsername);
+        });
+        console.log("count: " + cnt.data().count);
+        setTotalBtn(cnt.data().count);
+
+        const lastBtn = data?.docs[data.docs.length - 1];
+        setLastButton(lastBtn);
+        // localStorage.setItem(BUTTONS_CACHE_KEY, JSON.stringify(buttonsData));
+        return buttonsData;
+    };
+
+    const getMoreButtonsData = async () => {
+        // const cachedData = localStorage.getItem(BUTTONS_CACHE_KEY);
+        // if (cachedData) {
+        //     return JSON.parse(cachedData);
+        // }
+
+        // const buttonCollectionRef = collection(db, "buttons");
+        // const data = await getDocs(buttonCollectionRef);
+
+        const buttonCollectionRef = collection(db, "buttons");
+        let snapshot = query(
+            buttonCollectionRef,
+            orderBy("likeCounter", "desc"),
+            limit(6),
+            startAfter(lastButton)
+        );
+        const data = await getDocs(snapshot);
+
+        const newBtn = data.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+        }));
+
+        const lastBtn = data?.docs[data.docs.length - 1];
+        setLastButton(lastBtn);
+        // localStorage.setItem(BUTTONS_CACHE_KEY, JSON.stringify(buttonsData));
+        setButtonsData([...buttonsData, ...newBtn]);
+        buttonsData.map((item) => {
+            console.log("btnnnnnnnn----datttt", item.githubUsername);
+        });
+    };
+
+    // changes by aditya
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const buttonsData = await getButtonsData();
+                const usersData = await getUsersData();
+                setButtonsData(buttonsData);
+                setUsersData(usersData);
+            } catch (error) {
+                console.log("Error fetching data:", error);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const routes = [
+        {
+            path: "/",
+            element: (
+                <>
+                    <Landing
+                        modeToggle={toggleMode}
+                        modeToggleFunc={setToggleMode}
+                    />
+                    <Main
+                        modeToggle={toggleMode}
+                        modeToggleFunc={setToggleMode}
+                        buttonsData={buttonsData}
+                        fetchMoreData={getMoreButtonsData}
+                        setButtonsData={setButtonsData}
+                        totalBtn={totalBtn}
+                    />
+                </>
+            ),
+        },
+        {
+            path: "/about",
+            element: <About modeToggle={toggleMode} />,
+        },
+        {
+            path: "/show/:id",
+            element: (
+                <Suspense fallback={<SuspenseLoader />}>
+                    <ShowCode />
+                </Suspense>
+            ),
+        },
+        {
+            path: "/add",
+            element: <AddButton />,
+        },
+        // {
+        //   path: "/login",
+        //   element: <Login />,
+        // },
+        {
+            path: "/user/:userId",
+            element: <UserProfile modeToggle={toggleMode} />,
+        },
+        {
+            path: "/leaderboard",
+            element: <Score buttonsData={buttonsData} usersData={usersData} />,
+        },
+        {
+            path: "/explore",
+            element: (
+                <ExploreButtons data={buttonsData} toggleMode={toggleMode} />
+            ),
+        },
+        {
+            path: "*",
+            element: <ErrorPage modeToggleFunc={modeToggle} />,
+        },
+    ];
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 500);
+    }, []);
+
+    return (
         <>
-          <Landing modeToggle={toggleMode} modeToggleFunc={setToggleMode} />
-          <Main
-            modeToggle={toggleMode}
-            modeToggleFunc={setToggleMode}
-            buttonsData={buttonsData}
-          />
+            {loading ? (
+                <Loader />
+            ) : (
+                <div className={`${toggleMode ? "dark" : "light"}`}>
+                    <Navbar
+                        modeToggle={toggleMode}
+                        modeToggleFunc={setToggleMode}
+                    />
+                    <Routes>
+                        {routes.map((route, index) => (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={route.element}
+                            />
+                        ))}
+                    </Routes>
+                    <GoToTop />
+                    <Footer modeToggle={toggleMode} />
+                </div>
+            )}
         </>
-      ),
-    },
-    {
-      path: "/about",
-      element: <About modeToggle={toggleMode} />,
-    },
-    {
-      path: "/show/:id",
-      element: (
-        <Suspense fallback={<SuspenseLoader />}>
-          <ShowCode />
-        </Suspense>
-      ),
-    },
-    {
-      path: "/add",
-      element: <AddButton />,
-    },
-    // {
-    //   path: "/login",
-    //   element: <Login />,
-    // },
-    {
-      path: "/user/:userId",
-      element: <UserProfile modeToggle={toggleMode} />,
-    },
-    {
-      path: "/leaderboard",
-      element: <Score buttonsData={buttonsData} usersData={usersData} />,
-    },
-    {
-      path: "/explore",
-      element: <ExploreButtons data={buttonsData} toggleMode={toggleMode}/>,
-    },
-    {
-      path: "*",
-      element: <ErrorPage modeToggleFunc={modeToggle} />,
-    },
-  ];
-
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      setLoading(false);
-    }, 500);
-  }, []);
-
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-        <div className={`${toggleMode ? "dark" : "light"}`}>
-          <Navbar modeToggle={toggleMode} modeToggleFunc={setToggleMode} />
-          <Routes>
-            {routes.map((route, index) => (
-              <Route key={index} path={route.path} element={route.element} />
-            ))}
-          </Routes>
-          <GoToTop />
-          <Footer modeToggle={toggleMode} />
-        </div>
-      )}
-    </>
-  );
+    );
 };
 
 export default App;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,121 +1,164 @@
 import React, { useState } from "react";
 import classes from "./Main.module.css";
 import Card from "../common/Card/Card";
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
 import { fadeIn, paraAnim } from "../Animation/motion";
 
-export default function Main({ modeToggle, modeToggleFunc, buttonsData }) {
-  const [currentPage, setCurrentPage] = useState(
-    parseInt(localStorage.getItem("current_page")) || 1
-  );
-  const itemsPerPage = 36;
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const isActive = (i) => (currentPage === i + 1 ? classes.active : "");
+import { getMoreButtonsData } from "../../Server/getButtons";
 
-  const currentItems = buttonsData.slice(
-    indexOfLastItem - itemsPerPage,
-    indexOfLastItem
-  );
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-    localStorage.setItem("current_page", pageNumber);
-    window.scrollTo({ top: 500, behavior: "smooth" });
-  };
+export default function Main({
+    modeToggle,
+    modeToggleFunc,
+    buttonsData,
+    fetchMoreData,
+    setButtonsData,
+    totalBtn,
+}) {
+    const [currentPage, setCurrentPage] = useState(
+        parseInt(localStorage.getItem("current_page")) || 1
+    );
+    const itemsPerPage = 6;
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const isActive = (i) => (currentPage === i + 1 ? classes.active : "");
 
-  const pageNavigationButtions = (
-    <ul className={classes.paginationList}>
-      <li
-        className={`${classes.paginationItem} ${isActive(0)}`}
-        onClick={() => handlePageChange(1)}
-      >
-        {"<"}
-      </li>
-      {Array(Math.ceil(buttonsData.length / itemsPerPage))
-        .fill()
-        .map((_, index) => {
-          const pageNumber = index + 1;
-          if (
-            pageNumber === 1 ||
-            pageNumber === currentPage ||
-            (pageNumber >= currentPage - 1 && pageNumber <= currentPage + 1) ||
-            pageNumber === Math.ceil(buttonsData.length / itemsPerPage)
-          ) {
-            return (
-              <li
-                key={index}
-                className={`${classes.paginationItem} ${isActive(index)}`}
-                onClick={() => handlePageChange(pageNumber)}
-              >
-                {pageNumber}
-              </li>
-            );
-          } else if (
-            (pageNumber === currentPage - 2 && pageNumber > 1) ||
-            (pageNumber === currentPage + 2 &&
-              pageNumber < Math.ceil(buttonsData.length / itemsPerPage))
-          ) {
-            return (
-              <li
-                key={index}
-                className={`${classes.paginationItem} ${classes.ellipsis}`}
-                onClick={() => handlePageChange(pageNumber)}
-              >
-                ...
-              </li>
-            );
-          }
-          return null;
-        })}
-      <li
-        className={`${classes.paginationItem} ${isActive(
-          Math.ceil(buttonsData.length / itemsPerPage) - 1
-        )}`}
-        onClick={() =>
-          handlePageChange(Math.ceil(buttonsData.length / itemsPerPage))
+    const currentItems = buttonsData.slice(
+        indexOfLastItem - itemsPerPage,
+        indexOfLastItem
+    );
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+        localStorage.setItem("current_page", pageNumber);
+        window.scrollTo({ top: 500, behavior: "smooth" });
+    };
+
+    // // change by aditya
+    const handlePageChangeWithFetchMore = async (pageNumber) => {
+        // if(buttonsData.length)
+        try {
+            await fetchMoreData();
+        } catch (error) {
+            console.log("Error fetching data:", error);
         }
-      >
-        {">"}
-      </li>
-    </ul>
-  );
 
-  return (
-    <div className={classes.main_container}>
+        setCurrentPage(pageNumber);
+        localStorage.setItem("current_page", pageNumber);
+        window.scrollTo({ top: 500, behavior: "smooth" });
+    };
 
+    // change by aditya
 
-      {(buttonsData.length === 0) ?
+    const pageNavigationButtions = (
+        <ul className={classes.paginationList}>
+            <li
+                className={`${classes.paginationItem} ${isActive(0)}`}
+                onClick={() => handlePageChange(currentPage - 1)}
+            >
+                {"<"}
+            </li>
+            {Array(Math.ceil(buttonsData.length / itemsPerPage))
+                .fill()
+                .map((_, index) => {
+                    const pageNumber = index + 1;
+                    if (
+                        pageNumber === 1 ||
+                        pageNumber === currentPage ||
+                        (pageNumber >= currentPage - 1 &&
+                            pageNumber <= currentPage + 1) ||
+                        pageNumber ===
+                            Math.ceil(buttonsData.length / itemsPerPage)
+                    ) {
+                        return (
+                            <li
+                                key={index}
+                                className={`${
+                                    classes.paginationItem
+                                } ${isActive(index)}`}
+                                onClick={() => handlePageChange(pageNumber)}
+                            >
+                                {pageNumber}
+                            </li>
+                        );
+                    } else if (
+                        (pageNumber === currentPage - 2 && pageNumber > 1) ||
+                        (pageNumber === currentPage + 2 &&
+                            pageNumber <
+                                Math.ceil(buttonsData.length / itemsPerPage))
+                    ) {
+                        return (
+                            <li
+                                key={index}
+                                className={`${classes.paginationItem} ${classes.ellipsis}`}
+                                onClick={() => handlePageChange(pageNumber)}
+                            >
+                                ...
+                            </li>
+                        );
+                    }
+                    return null;
+                })}
+            <li
+                className={`${classes.paginationItem} ${isActive(
+                    Math.ceil(buttonsData.length / itemsPerPage) - 1
+                )}`}
+                onClick={() =>
+                    handlePageChangeWithFetchMore(
+                        // change by aditya
+                        // Math.ceil(buttonsData.length / itemsPerPage)
+                        currentPage + 1
+                    )
+                }
+            >
+                {">"}
+            </li>
+        </ul>
+    );
 
-        (<motion.h1
-          variants={paraAnim}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          transition={{ type: "spring", stiffness: 60 }}
-          className={classes.wait}>We Are Constantly Working To Provide You With The Best Possible Experience ... <br /><br /> Thank You For Your Patience 🫠 </motion.h1>) :
-
-        (<div><h1 style={{ textAlign: "center", marginTop: "30px" }}>
-          Total number of Buttons added {buttonsData.length}
-        </h1>
-          <div className={classes.btns_container}>
-            {currentItems.map((button, index) => (
-              <motion.div
-              key={index}
-              variants={fadeIn}
-              initial={'hidden'}
-              whileInView={'visible'}
-              viewport={{once : true}}
-              transition={{ duration : 0.5 ,delay : (index%3)*0.3}}
-              >
-                <Card modeToggle={modeToggle} key={index} button={button} />
-              </motion.div>
-            ))}
-          </div>
-          <div className={classes.pagination}>
-            {buttonsData.length > itemsPerPage && pageNavigationButtions}
-          </div>
-        </div>)
-      }
-
-    </div>
-  );
+    return (
+        <div className={classes.main_container}>
+            {totalBtn === 0 ? (
+                <motion.h1
+                    variants={paraAnim}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{ type: "spring", stiffness: 60 }}
+                    className={classes.wait}
+                >
+                    We Are Constantly Working To Provide You With The Best
+                    Possible Experience ... <br />
+                    <br /> Thank You For Your Patience 🫠{" "}
+                </motion.h1>
+            ) : (
+                <div>
+                    <h1 style={{ textAlign: "center", marginTop: "30px" }}>
+                        Total number of Buttons added {totalBtn}
+                    </h1>
+                    <div className={classes.btns_container}>
+                        {currentItems.map((button, index) => (
+                            <motion.div
+                                key={index}
+                                variants={fadeIn}
+                                initial={"hidden"}
+                                whileInView={"visible"}
+                                viewport={{ once: true }}
+                                transition={{
+                                    duration: 0.5,
+                                    delay: (index % 3) * 0.3,
+                                }}
+                            >
+                                <Card
+                                    modeToggle={modeToggle}
+                                    key={index}
+                                    button={button}
+                                />
+                            </motion.div>
+                        ))}
+                    </div>
+                    <div className={classes.pagination}>
+                        {totalBtn > itemsPerPage && pageNavigationButtions}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 }
